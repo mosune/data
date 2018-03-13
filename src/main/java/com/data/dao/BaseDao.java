659bc711de47c71141c284ec319f7eb72372d0fa
gcg <526409252@qq.com>
2018-03-13 16:11:20 +0800

    add basedao interface

---
+++ src/main/java/com/data/dao/BaseDao.java
@@ -0,0 +1,9 @@
+package com.data.dao;
+
+/**
+ * baseDao接口
+ *
+ * @author gcg
+ */
+public interface BaseDao<T> {
+}
--- src/main/java/com/data/dao/impl/BaseDaoImpl.java
+++ src/main/java/com/data/dao/impl/BaseDaoImpl.java
@@ -1,5 +1,6 @@
 package com.data.dao.impl;
 
+import com.data.dao.BaseDao;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.support.SqlSessionDaoSupport;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
  *
  * @author gcg
  **/
-public class BaseDaoImpl<T> extends SqlSessionDaoSupport {
+public class BaseDaoImpl<T> extends SqlSessionDaoSupport implements BaseDao<T> {
 
 	@Autowired
 	public void setSqlSessionFactory(SqlSessionFactory sqlSessionFactory) {
